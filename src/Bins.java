8b251aeeaa526ed2612de246b5a95f0b2a4578ae
Connor Usry <cgu1001@gmail.com>
2015-08-27 20:50:28 -0400

    Refactors bin file to remove 'code smells'

--- src/Bins.java
+++ src/Bins.java
@@ -10,7 +10,14 @@ import java.util.Scanner;
  * Runs a number of algorithms that try to fit files onto disks.
  */
 public class Bins {
-    public static final String DATA_FILE = "example.txt";
+    
+	//Make variables used in abstracted methods global
+	private static int total;
+    private static PriorityQueue<Disk> pq; 
+    private static List<Integer> data; 
+    private static int diskId;
+    
+	public static final String DATA_FILE = "example.txt";
 
     /**
      * Reads list of integer data from the given input.
@@ -32,61 +39,60 @@ public class Bins {
     public static void main (String args[]) {
         Bins b = new Bins();
         Scanner input = new Scanner(Bins.class.getClassLoader().getResourceAsStream(DATA_FILE));
-        List<Integer> data = b.readData(input);
+        data = b.readData(input);
 
-        PriorityQueue<Disk> pq = new PriorityQueue<Disk>();
-        pq.add(new Disk(0));
-
-        int diskId = 1;
-        int total = 0;
-        for (Integer size : data) {
-            Disk d = pq.peek();
-            if (d.freeSpace() > size) {
-                pq.poll();
-                d.add(size);
-                pq.add(d);
-            } else {
-                Disk d2 = new Disk(diskId);
-                diskId++;
-                d2.add(size);
-                pq.add(d2);
-            }
-            total += size;
-        }
-
-        System.out.println("total size = " + total / 1000000.0 + "GB");
-        System.out.println();
-        System.out.println("worst-fit method");
-        System.out.println("number of pq used: " + pq.size());
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
-        System.out.println();
-
-        Collections.sort(data, Collections.reverseOrder());
-        pq.add(new Disk(0));
-
-        diskId = 1;
-        for (Integer size : data) {
-            Disk d = pq.peek();
-            if (d.freeSpace() >= size) {
-                pq.poll();
-                d.add(size);
-                pq.add(d);
-            } else {
-                Disk d2 = new Disk(diskId);
-                diskId++;
-                d2.add(size);
-                pq.add(d2);
-            }
-        }
-
-        System.out.println();
-        System.out.println("worst-fit decreasing method");
-        System.out.println("number of pq used: " + pq.size());
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
-        System.out.println();
+        pq = new PriorityQueue<Disk>();
+        
+        //insert without sorting
+        binInsertion(false);
+        // Include a line insertion instead of separate system.out.println
+        System.out.println("total size = " + total / 1000000.0 + "GB \n");
+        printMethodResults("worst-fit");
+        printBinAndDelete();
+        
+        
+        //insert after sorting
+        binInsertion(true);
+        printMethodResults("worst-fit decreasing");
+        printBinAndDelete();
     }
+
+
+    //Method to perform the bin insertion, varying depending on whether or not we sort 
+    //the collection beforehand
+    private static void binInsertion(boolean sort) {
+    	if(sort){ Collections.sort(data, Collections.reverseOrder());}
+    	
+    	pq.add(new Disk(0));
+    	diskId = 1;
+    	total = 0;
+    	for (Integer size : data) {
+    		Disk d = pq.peek();
+    		if (d.freeSpace() > size || (sort && d.freeSpace() >= size)) {
+    			pq.poll();
+    			d.add(size);
+    			pq.add(d);
+    		} else {
+    			Disk d2 = new Disk(diskId);
+    			diskId++;
+    			d2.add(size);
+    			pq.add(d2);
+    		}
+    		total += size;
+    	}
+    }
+    
+    //Method that prints the method results 
+	private static void printMethodResults(String methodType) {
+		System.out.println(methodType + " method");
+        System.out.println("number of pq used: " + pq.size());
+	}
+	
+	//Method that deletes the priority queue and prints contents 
+	private static void printBinAndDelete() {
+		while (!pq.isEmpty()) {
+			System.out.println(pq.poll());
+		}
+		System.out.println();
+	}
 }

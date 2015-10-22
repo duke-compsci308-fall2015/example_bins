8a75480388e5fd1dfa9fdd5172f4750643774f88
sallymerza <sma45@duke.edu>
2015-10-22 16:56:03 -0400

    sma45 and dbm33

--- src/Bins.java
+++ src/Bins.java
@@ -1,57 +1,63 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
 
 /**
  * Runs a number of algorithms that try to fit files onto disks.
  */
 public class Bins {
-    public static final String DATA_FILE = "example.txt";
-    // all possible algorithms to compare --- add new instances here!
-    private static WorstFitAlgorithm algortihmsToCompare[] = { 
-        new WorstFitAlgorithm(),
-        new WorstFitDecreasingAlgorithm()
-    };
+	public static final String DATA_FILE = "example.txt";
+	// all possible algorithms to compare --- add new instances here!
+	/*
+	 * private static WorstFitAlgorithm algortihmsToCompare[] = { new
+	 * WorstFitAlgorithm(), new WorstFitDecreasingAlgorithm() };
+	 */
 
+	/**
+	 * Reads list of integer data from the given input.
+	 *
+	 * @param input
+	 *            tied to an input source that contains space separated numbers
+	 * @return list of the numbers in the order they were read
+	 */
+	public List<Integer> readData(Scanner input) {
+		List<Integer> results = new ArrayList<Integer>();
+		while (input.hasNext()) {
+			results.add(input.nextInt());
+		}
+		return results;
+	}
 
-    /**
-     * Reads list of integer data from the given input.
-     * 
-     * @param input tied to an input source that contains space separated numbers
-     * @return list of the numbers in the order they were read
-     */
-    public List<Integer> readData (Scanner input) {
-        List<Integer> results = new ArrayList<Integer>();
-        while (input.hasNext()) {
-            results.add(input.nextInt());
-        }
-        return results;
-    }
+	/**
+	 * Returns sum of all values in given list.
+	 */
+	public int getTotal(List<Integer> data) {
+		int total = 0;
+		for (int d : data) {
+			total += d;
+		}
+		return total;
+	}
 
-    /**
-     * Returns sum of all values in given list.
-     */
-    public int getTotal (List<Integer> data) {
-        int total = 0;
-        for (int d : data) {
-            total += d;
-        }
-        return total;
-    }
+	/**
+	 * The main program.
+	 */
+	public static void main(String args[]) {
+		Bins b = new Bins();
+		Scanner input = new Scanner(Bins.class.getClassLoader().getResourceAsStream(DATA_FILE));
+		List<Integer> data = b.readData(input);
+		System.out.println("total size = " + b.getTotal(data) / 1000000.0 + "GB");
 
+		WorstFitAlgorithm al = new WorstFitAlgorithm();
+		// worst fit
+		al.fitDisksAndPrint(data, dataList -> dataList);
+		// decreasing
 
-    /**
-     * The main program.
-     */
-    public static void main (String args[]) {
-        Bins b = new Bins();
-        Scanner input = new Scanner(Bins.class.getClassLoader().getResourceAsStream(DATA_FILE));
-        List<Integer> data = b.readData(input);
-        System.out.println("total size = " + b.getTotal(data) / 1000000.0 + "GB");
+		al.fitDisksAndPrint(data, dataList -> {
+			Collections.sort(dataList, Collections.reverseOrder());
+			return dataList;
+		});
 
-        for (WorstFitAlgorithm al : algortihmsToCompare) {
-            al.fitDisksAndPrint(data);
-        }
-    }
+	}
 }
---
+++ src/StaticAbstractMultiInheritedGlobalInterface.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ *
+ */
+
+/**
+ *
+ * @author Sally Al
+ *
+ */
+
+@FunctionalInterface
+public interface StaticAbstractMultiInheritedGlobalInterface {
+	List<Integer> operate(List<Integer> dataList);
+
+}
